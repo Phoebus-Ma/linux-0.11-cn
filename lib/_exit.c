ff49f78de81faaeef59c49acb460a1fc0a7203da
Phoebus <tianjindahai@126.com>
2023-04-09 11:04:24 +0800

    [kernel] Update lib folder.

--- lib/_exit.c
+++ lib/_exit.c
@@ -7,7 +7,13 @@
 #define __LIBRARY__
 #include <unistd.h>
 
+/**
+ * 内核使用的程序(退出)终止函数.
+ * 直接调用系统中断int 0x80，功能号__NR_exit.
+ * 参数：exit_code - 退出码.
+ */
 volatile void _exit(int exit_code)
 {
+    /* %0 - eax(系统调用号__NR_exit)；%1 - ebx(退出码 exit_code). */
     __asm__("int $0x80" ::"a"(__NR_exit), "b"(exit_code));
 }
--- lib/dup.c
+++ lib/dup.c
@@ -7,4 +7,9 @@
 #define __LIBRARY__
 #include <unistd.h>
 
+/**
+ * 复制文件描述符函数。
+ * 下面该调用宏函数对应：int dup(int fd)。直接调用了系统中断int 0x80，参数是__NR_dup。
+ * 其中fd是文件描述符.
+ */
 _syscall1(int, dup, int, fd)
