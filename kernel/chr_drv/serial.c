9f627c9cfe9a78a03309c32b8b335abd31e587bb
Phoebus <tianjindahai@126.com>
2023-05-09 00:05:03 +0800

    [kernel] Update serial.c tty_ioctl.c.

--- kernel/chr_drv/serial.c
+++ kernel/chr_drv/serial.c
@@ -5,12 +5,12 @@
  */
 
 /*
- *	serial.c
+ * serial.c
  *
- * This module implements the rs232 io functions
- *	void rs_write(struct tty_struct * queue);
- *	void rs_init(void);
- * and all interrupts pertaining to serial IO.
+ * 该程序用于实现rs232的输入输出功能.
+ *  void rs_write(struct tty_struct * queue);
+ *  void rs_init(void);
+ * 以及与传输 IO 有关系的所有中断处理程序.
  */
 
 #include <linux/tty.h>
@@ -18,44 +18,70 @@
 #include <asm/system.h>
 #include <asm/io.h>
 
+/* 当写队列中含有WAKEUP_CHARS个字符时，就开始发送. */
 #define WAKEUP_CHARS            (TTY_BUF_SIZE / 4)
 
+/* 串行口1的中断处理程序(rs_io.s). */
 extern void rs1_interrupt(void);
+/* 串行口2的中断处理程序(rs_io.s). */
 extern void rs2_interrupt(void);
 
+/**
+ * init - 初始化串行端口.
+ * port: 串口 1 - 0x3F8，串口 2 - 0x2F8.
+ */
 static void init(int port)
 {
-    outb_p(0x80, port + 3); /* set DLAB of line control reg */
-    outb_p(0x30, port);     /* LS of divisor (48 -> 2400 bps */
-    outb_p(0x00, port + 1); /* MS of divisor */
-    outb_p(0x03, port + 3); /* reset DLAB */
-    outb_p(0x0b, port + 4); /* set DTR,RTS, OUT_2 */
-    outb_p(0x0d, port + 1); /* enable all intrs but writes */
-    (void)inb(port);        /* read data port to reset things (?) */
+    outb_p(0x80, port + 3);     /* 设置线路控制寄存器的DLAB位(位7). */
+    outb_p(0x30, port);         /* 发送波特率因子低字节，0x30->2400bps. */
+    outb_p(0x00, port + 1);     /* 发送波特率因子高字节，0x00. */
+    outb_p(0x03, port + 3);     /* 复位DLAB位，数据位为8位. */
+    outb_p(0x0b, port + 4);     /* 设置DTR，RTS，辅助用户输出2. */
+    outb_p(0x0d, port + 1);     /* 除了写(写保持空)以外，允许所有中断源中断. */
+
+    (void)inb(port);            /* 读数据口，以进行复位操作(?). */
 }
 
+/**
+ * rs_init - 初始化串行中断程序和串行接口.
+ */
 void rs_init(void)
 {
+    /* 设置串行口1的中断门向量(硬件IRQ4信号). */
     set_intr_gate(0x24, rs1_interrupt);
+    /* 设置串行口2的中断门向量(硬件IRQ3信号). */
     set_intr_gate(0x23, rs2_interrupt);
+
+    /* 初始化串行口1(.data是端口号). */
     init(tty_table[1].read_q.data);
+    /* 初始化串行口2. */
     init(tty_table[2].read_q.data);
+
+    /* 允许主8259A芯片的IRQ3，IRQ4中断信号请求. */
     outb(inb_p(0x21) & 0xE7, 0x21);
 }
 
 /*
- * This routine gets called when tty_write has put something into
- * the write_queue. It must check wheter the queue is empty, and
- * set the interrupt register accordingly
- *
- *	void _rs_write(struct tty_struct * tty);
+ * 在 tty_write()已将数据放入输出(写)队列时会调用下面的子程序。必须首先
+ * 检查写队列是否为空，并相应设置中断寄存器.
  */
+/**
+ * rs_write - 串行数据发送输出。
+ * 实际上只是开启串行发送保持寄存器已空中断标志，在UART将数据发送出去后
+ * 允许发中断信号.
+*/
 void rs_write(struct tty_struct *tty)
 {
+    /* 关中断. */
     cli();
 
+    /**
+     * 如果写队列不空，则从0x3f9(或0x2f9)首先读取中断允许寄存器内容，
+     * 添上发送保持寄存器中断允许标志(位1)后，再写回该寄存器.
+     */
     if (!EMPTY(tty->write_q))
         outb(inb_p(tty->write_q.data + 1) | 0x02, tty->write_q.data + 1);
 
+    /* 开中断. */
     sti();
 }
--- kernel/chr_drv/tty_ioctl.c
+++ kernel/chr_drv/tty_ioctl.c
@@ -15,31 +15,57 @@
 #include <asm/segment.h>
 #include <asm/system.h>
 
+/**
+ * 这是波特率因子数组(或称为除数数组)。
+ * 波特率与波特率因子的对应关系参见列表后的说明.
+ */
 static unsigned short quotient[] = {
     0,      2304,   1536,   1047,   857,
     768,    576,    384,    192,    96,
     64,     48,     24,     12,     6,      3
 };
 
+/**
+ * change_speed - 修改传输速率。
+ * 参数：tty - 终端对应的tty数据结构。
+ * 在除数锁存标志 DLAB(线路控制寄存器位7)置位情况下，通过端口
+ * 0x3f8和0x3f9向UART分别写入波特率因子低字节和高字节.
+ */
 static void change_speed(struct tty_struct *tty)
 {
     unsigned short port, quot;
 
+    /** 
+     * 对于串口终端，其tty结构的读缓冲队列data字段存放的是串行端口号
+     * (0x3f8或0x2f8).
+     */
     if (!(port = tty->read_q.data))
         return;
 
+    /**
+     * 从tty的termios结构控制模式标志集中取得设置的波特率索引号，据此
+     * 从波特率因子数组中取得对应的波特率因子值。CBAUD 是控制模式标志
+     * 集中波特率位屏蔽码.
+     */
     quot = quotient[tty->termios.c_cflag & CBAUD];
 
+    /* 关中断. */
     cli();
 
-    outb_p(0x80, port + 3);      /* set DLAB */
-    outb_p(quot & 0xff, port);   /* LS of divisor */
-    outb_p(quot >> 8, port + 1); /* MS of divisor */
-    outb(0x03, port + 3);        /* reset DLAB */
+    outb_p(0x80, port + 3);      /* 首先设置除数锁定标志DLAB. */
+    outb_p(quot & 0xff, port);   /* 输出因子低字节. */
+    outb_p(quot >> 8, port + 1); /* 输出因子高字节. */
+    outb(0x03, port + 3);        /* 复位DLAB. */
 
+    /* 开中断. */
     sti();
 }
 
+/**
+ * flush - 刷新tty缓冲队列。
+ * 参数：gueue - 指定的缓冲队列指针。
+ * 令缓冲队列的头指针等于尾指针，从而达到清空缓冲区(零字符)的目的.
+ */
 static void flush(struct tty_queue *queue)
 {
     cli();
@@ -49,55 +75,106 @@ static void flush(struct tty_queue *queue)
     sti();
 }
 
+/**
+ * wait_until_sent - 等待字符发送出去.
+ */
 static void wait_until_sent(struct tty_struct *tty)
 {
     /* do nothing - not implemented */
 }
 
+/**
+ * send_break - 发送BREAK控制符.
+ */
 static void send_break(struct tty_struct *tty)
 {
     /* do nothing - not implemented */
 }
 
+/**
+ * get_termios - 取终端termios结构信息。
+ * 
+ * 参数：
+ * tty - 指定终端的tty结构指针；
+ * termios - 用户数据区termios结构缓冲区指针。
+ * 
+ * 返回0.
+ */
 static int get_termios(struct tty_struct *tty, struct termios *termios)
 {
     int i;
 
+    /* 首先验证一下用户的缓冲区指针所指内存区是否足够，如不够则分配内存. */
     verify_area(termios, sizeof(*termios));
 
+    /* 复制指定tty结构中的termios结构信息到用户termios结构缓冲区. */
     for (i = 0; i < (sizeof(*termios)); i++)
         put_fs_byte(((char *)&tty->termios)[i], i + (char *)termios);
 
     return 0;
 }
 
+/**
+ * set_termios - 设置终端termios结构信息.
+ * 
+ * 参数：
+ * tty - 指定终端的tty结构指针；
+ * termios - 用户数据区termios结构指针。
+ * 
+ * 返回0.
+ */
 static int set_termios(struct tty_struct *tty, struct termios *termios)
 {
     int i;
 
+    /* 首先复制用户数据区中termios结构信息到指定tty结构中. */
     for (i = 0; i < (sizeof(*termios)); i++)
         ((char *)&tty->termios)[i] = get_fs_byte(i + (char *)termios);
 
+    /**
+     * 用户有可能已修改了tty的串行口传输波特率，所以根据termios结构中的
+     * 控制模式标志c_cflag修改串行芯片UART的传输波特率.
+     */
     change_speed(tty);
 
     return 0;
 }
 
+/**
+ * get_termio - 读取termio结构中的信息。
+ * 
+ * 参数：
+ * tty - 指定终端的tty结构指针；
+ * termio - 用户数据区termio结构缓冲区指针。
+ * 
+ * 返回0.
+ */
 static int get_termio(struct tty_struct *tty, struct termio *termio)
 {
     int i;
     struct termio tmp_termio;
 
+    /**
+     * 首先验证一下用户的缓冲区指针所指内存区是否足够，如不够则分配内存.
+     */
     verify_area(termio, sizeof(*termio));
+
+    /**
+     * 将termios结构的信息复制到termio结构中。目的是为了其中模式标志集的
+     * 类型进行转换，也即从termios的长整数类型转换为termio的短整数类型.
+     */
     tmp_termio.c_iflag = tty->termios.c_iflag;
     tmp_termio.c_oflag = tty->termios.c_oflag;
     tmp_termio.c_cflag = tty->termios.c_cflag;
     tmp_termio.c_lflag = tty->termios.c_lflag;
+
+    /* 两种结构的c_line和c_cc[]字段是完全相同的. */
     tmp_termio.c_line = tty->termios.c_line;
 
     for (i = 0; i < NCC; i++)
         tmp_termio.c_cc[i] = tty->termios.c_cc[i];
 
+    /* 最后复制指定tty结构中的termio结构信息到用户termio结构缓冲区. */
     for (i = 0; i < (sizeof(*termio)); i++)
         put_fs_byte(((char *)&tmp_termio)[i], i + (char *)termio);
 
@@ -105,34 +182,63 @@ static int get_termio(struct tty_struct *tty, struct termio *termio)
 }
 
 /*
- * This only works as the 386 is low-byt-first
+ * 下面的termio设置函数仅在386低字节在前的方式下可用.
  */
+/**
+ * set_termio - 设置终端termio结构信息.
+ *
+ * 参数：
+ * tty - 指定终端的tty结构指针；
+ * termio - 用户数据区termio结构指针。
+ * 
+ * 将用户缓冲区termio的信息复制到终端的termios结构中。返回0.
+*/
 static int set_termio(struct tty_struct *tty, struct termio *termio)
 {
     int i;
     struct termio tmp_termio;
 
+    /* 首先复制用户数据区中termio结构信息到临时termio结构中. */
     for (i = 0; i < (sizeof(*termio)); i++)
         ((char *)&tmp_termio)[i] = get_fs_byte(i + (char *)termio);
 
+    /**
+     * 再将termio结构的信息复制到tty的termios结构中。目的是为了其中模式
+     * 标志集的类型进行转换，也即从termio的短整数类型转换成termios的长整数类型.
+     */
     *(unsigned short *)&tty->termios.c_iflag = tmp_termio.c_iflag;
     *(unsigned short *)&tty->termios.c_oflag = tmp_termio.c_oflag;
     *(unsigned short *)&tty->termios.c_cflag = tmp_termio.c_cflag;
     *(unsigned short *)&tty->termios.c_lflag = tmp_termio.c_lflag;
+    
+    /* 两种结构的c_line和c_cc[]字段是完全相同的. */
     tty->termios.c_line = tmp_termio.c_line;
 
     for (i = 0; i < NCC; i++)
         tty->termios.c_cc[i] = tmp_termio.c_cc[i];
 
+    /**
+     * 用户可能已修改了tty的串行口传输波特率，所以根据termios结构中的控制模式
+     * 标志集c_cflag修改串行芯片UART的传输波特率.
+     */
     change_speed(tty);
 
     return 0;
 }
 
+/**
+ * tty_ioctl - tty终端设备的ioctl函数。
+ * 参数：dev - 设备号；cmd - ioctl 命令；arg - 操作参数指针.
+ */
 int tty_ioctl(int dev, int cmd, int arg)
 {
     struct tty_struct *tty;
 
+    /**
+     * 首先取tty的子设备号。如果主设备号是5(tty终端)，则进程的tty字段即是子设备号；
+     * 如果进程的tty子设备号是负数，表明该进程没有控制终端，也即不能发出该ioctl调用，
+     * 出错死机.
+     */
     if (MAJOR(dev) == 5)
     {
         dev = current->tty;
@@ -140,38 +246,80 @@ int tty_ioctl(int dev, int cmd, int arg)
         if (dev < 0)
             panic("tty_ioctl: dev<0");
     }
+    /* 否则直接从设备号中取出子设备号. */
     else
         dev = MINOR(dev);
+
+    /**
+     * 子设备号可以是0(控制台终端)、1(串口1终端)、2(串口2终端)。
+     * 让tty指向对应子设备号的tty结构.
+     */
     tty = dev + tty_table;
 
+    /* 根据tty的ioctl命令进行分别处理. */
     switch (cmd)
     {
     case TCGETS:
+        /* 取相应终端 termios 结构中的信息. */
         return get_termios(tty, (struct termios *)arg);
+
     case TCSETSF:
-        flush(&tty->read_q); /* fallthrough */
+        /**
+         * 在设置termios的信息之前，需要先等待输出队列中所有数据处理完，
+         * 并且刷新(清空)输入队列.
+         */
+        flush(&tty->read_q); /* 继续执行. */
     case TCSETSW:
-        wait_until_sent(tty); /* fallthrough */
+        /**
+         * 在设置终端termios的信息之前，需要先等待输出队列中所有数据处理完
+         * (耗尽)。对于修改参数会影响输出的情况，就需要使用这种形式.
+         */
+        wait_until_sent(tty); /* 继续执行. */
     case TCSETS:
+        /* 设置相应终端termios结构中的信息. */
         return set_termios(tty, (struct termios *)arg);
+
     case TCGETA:
+        /* 取相应终端termio结构中的信息. */
         return get_termio(tty, (struct termio *)arg);
+
     case TCSETAF:
-        flush(&tty->read_q); /* fallthrough */
+        /**
+         * 在设置termio的信息之前，需要先等待输出队列中所有数据处理完，
+         * 并且刷新(清空)输入队列。再设置.
+         */
+        flush(&tty->read_q); /* 继续执行. */
     case TCSETAW:
-        wait_until_sent(tty); /* fallthrough */
+        /**
+         * 在设置终端termio的信息之前，需要先等待输出队列中所有数据处理完
+         * (耗尽)。对于修改参数会影响输出的情况，就需要使用这种形式.
+         */
+        wait_until_sent(tty); /* 继续执行. */
     case TCSETA:
+        /* 设置相应终端termio结构中的信息. */
         return set_termio(tty, (struct termio *)arg);
+
     case TCSBRK:
+        /* 等待输出队列处理完毕(空)，如果参数值是0，则发送一个break. */
         if (!arg)
         {
             wait_until_sent(tty);
             send_break(tty);
         }
         return 0;
+
     case TCXONC:
+        /**
+         * 开始/停止控制。如果参数值是0，则挂起输出；如果是1，则重新开启挂起的输出；
+         * 如果是 2，则挂起输入；如果是 3，则重新开启挂起的输入.
+         */
         return -EINVAL; /* not implemented */
+
     case TCFLSH:
+        /**
+         * 刷新已写输出但还没发送或已收但还没有读数据。如果参数是0，则刷新(清空)输入队列；
+         * 如果是1，则刷新输出队列；如果是2，则刷新输入和输出队列.
+         */
         if (arg == 0)
             flush(&tty->read_q);
         else if (arg == 1)
@@ -184,46 +332,94 @@ int tty_ioctl(int dev, int cmd, int arg)
         else
             return -EINVAL;
         return 0;
+
     case TIOCEXCL:
+        /* 设置终端串行线路专用模式. */
         return -EINVAL; /* not implemented */
+
     case TIOCNXCL:
+        /* 复位终端串行线路专用模式. */
         return -EINVAL; /* not implemented */
+
     case TIOCSCTTY:
+        /* 设置tty为控制终端. */
         return -EINVAL; /* set controlling term NI */
+
     case TIOCGPGRP:
+        /**
+         * 读取指定终端设备进程的组id。首先验证用户缓冲区长度，然后复制tty
+         * 的pgrp字段到用户缓冲区。
+         */
         verify_area((void *)arg, 4);
         put_fs_long(tty->pgrp, (unsigned long *)arg);
         return 0;
+
     case TIOCSPGRP:
+        /* 设置指定终端设备进程的组id. */
         tty->pgrp = get_fs_long((unsigned long *)arg);
         return 0;
+
     case TIOCOUTQ:
+        /**
+         * 返回输出队列中还未送出的字符数。首先验证用户缓冲区长度，
+         * 然后复制队列中字符数给用户. 
+         */
         verify_area((void *)arg, 4);
         put_fs_long(CHARS(tty->write_q), (unsigned long *)arg);
         return 0;
+
     case TIOCINQ:
+        /**
+         * 返回输入队列中还未读取的字符数。首先验证用户缓冲区长度，
+         * 然后复制队列中字符数给用户。
+         */
         verify_area((void *)arg, 4);
         put_fs_long(CHARS(tty->secondary),
                     (unsigned long *)arg);
         return 0;
+
     case TIOCSTI:
+        /**
+         * 模拟终端输入。该命令以一个指向字符的指针作为参数，并假装该字符是在
+         * 终端上键入的。用户必须在该控制终端上具有超级用户权限或具有读许可权限.
+         */
         return -EINVAL; /* not implemented */
+
     case TIOCGWINSZ:
+        /* 读取终端设备窗口大小信息(参见termios.h中的winsize结构). */
         return -EINVAL; /* not implemented */
+
     case TIOCSWINSZ:
+        /* 设置终端设备窗口大小信息(参见winsize结构). */
         return -EINVAL; /* not implemented */
+
     case TIOCMGET:
+        /* 返回modem状态控制引线的当前状态比特位标志集(参见termios.h). */
         return -EINVAL; /* not implemented */
+
     case TIOCMBIS:
+        /* 设置单个modem状态控制引线的状态(true或false). */
         return -EINVAL; /* not implemented */
+
     case TIOCMBIC:
+        /* 复位单个modem状态控制引线的状态. */
         return -EINVAL; /* not implemented */
+
     case TIOCMSET:
+        /**
+         * 设置modem状态引线的状态。如果某一比特位置位，则modem对应的状态
+         * 引线将置为有效.
+         */
         return -EINVAL; /* not implemented */
+
     case TIOCGSOFTCAR:
+        /* 读取软件载波检测标志(1 - 开启；0 - 关闭). */
         return -EINVAL; /* not implemented */
+
     case TIOCSSOFTCAR:
+        /* 设置软件载波检测标志(1 - 开启；0 - 关闭). */
         return -EINVAL; /* not implemented */
+
     default:
         return -EINVAL;
     }
